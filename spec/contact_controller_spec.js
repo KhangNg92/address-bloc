cf2025ac7cee733e6382427cf1f59dc16c9ca590
mkavo92 <mkavo92@gmail.com>
2018-07-30 13:25:56 -0500

    add git ignore

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -19,7 +19,7 @@ describe("ContactController", () => {
   describe("#addContact()", () => {
 
     it("should add a single contact into the book", (done) => {
-        this.book.addContact("Alice", "001-101-1010")
+        this.book.addContact("Alice", "001-101-1010","alice@gmail.com")
         .then((contact) => {
 
 // #3
@@ -32,6 +32,36 @@ describe("ContactController", () => {
           done();
         });
     });
+  });
+  describe("#getContacts()", () => {
+
+    it("should return an empty array when no contacts are available", (done) => {
+      this.book.getContacts()
+      .then((contacts) => {
+        expect(contacts.length).toBe(0);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
+
+    it("should return an array of contacts when contacts are available", (done) => {
+      this.book.addContact("Alice", "001-101-1010", "alice@example.com")
+      .then(() => {
+        this.book.getContacts()
+        .then((contacts) => {
+          expect(contacts.length).toBe(1);
+          done();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+    });
 
   });
+
 })
